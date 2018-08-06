25dd992b9ee38f576e090eda09ae9fb5d5dd3af6
Carlos Cana <carloscana@gmail.com>
2018-08-06 15:01:57 +0200

    Tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,19 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+def setup
+    @user = User.new(first_name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.first_name = "     "
+    assert_not @user.valid?
+  end
+
+
+
 end
