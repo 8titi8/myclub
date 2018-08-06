91f53d26b304137967294a54b2efe679b0f1e348
Pauline <mya@mya-ow.net>
2018-08-06 18:45:01 +0200

    suppression private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-
+    unless logged_in?
+      redirect_to login_path, :flash => { :danger => "Pour accéder à cette page, il faut d'abord vous connecter." }
+      end
   end
 
   # GET /users/new
