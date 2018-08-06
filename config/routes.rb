19e3e806356da12ffca0ce40ca0a75874307b3fa
laetitia69 <titi-fernandes@hotmail.fr>
2018-08-06 11:48:16 +0200

    dernieres modifs routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   root 'static_pages#home'
+  #rajout routes
   patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 end
