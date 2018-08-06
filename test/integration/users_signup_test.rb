5065bd4c5e99c66aebe9da72e66e4fc848538d18
Carlos Cana <carloscana@gmail.com>
2018-08-06 18:41:35 +0200

    Test signup finis

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,14 +5,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get new_user_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { first_name:  "",
-      									                 last_name: "",
+                                         last_name: "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
-
+    assert_select 'div', attributes: {id: "error_explanation"}
+    assert_select 'div.alert-danger'
   end
 end
