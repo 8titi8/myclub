4d91890858c026ee5e4d894c72e25aba78ff6c8b
jihane <thp@jihane.fr>
2018-08-07 01:11:02 +0200

    modification des tests de connexion

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+      @user = users(:one)
+  end
+
   test "invalid signup information" do
     get new_user_path
     assert_no_difference 'User.count' do
@@ -14,4 +19,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div', attributes: {id: "error_explanation"}
     assert_select 'div.alert-danger'
   end
+
+
+  test "valid signup information" do
+    get new_user_path
+    nb_before = User.count
+    post users_path, params: { user: { first_name:  "test",
+                                       last_name: "test",
+                                       email: "user@invalid.fr",
+                                       password:              "foofoo",
+                                       password_confirmation: "foofoo" } }
+    nb_after = User.count
+
+    assert (nb_before +1 ) == nb_after
+
+  end
+
+
 end
