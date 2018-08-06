3da6cfed58ebc69932596d2bf8cf2c0616b926b0
laetitia69 <titi-fernandes@hotmail.fr>
2018-08-06 11:45:12 +0200

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :users
   root 'static_pages#home'
   patch '/users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
+  delete '/users/:id', to: 'users#destroy'
 end
