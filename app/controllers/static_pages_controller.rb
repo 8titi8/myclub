3f41e140e5bf3859a7a14068dd47c035c8a42754
Pauline <mya@mya-ow.net>
2018-08-06 17:33:37 +0200

    accueil

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def private
+    unless logged_in?
+      redirect_to login_path, :flash => { :danger => "Pour accéder à cette page, il faut d'abord vous connecter." }
+      end
+  end
 end
