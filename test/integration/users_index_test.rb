d2fbd23cebd953ac343b1f744fd3c40a5a22f962
jihane <thp@jihane.fr>
2018-08-07 03:11:31 +0200

    update

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,5 +6,24 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  
+  test "unsuccessful index" do
+    get index_path
+    assert_redirected_to login_path
+    assert_not flash.empty?
+  end
+
+  test "successful index" do
+    log_in_as(@user)
+    get index_path
+    assert_template 'index'
+
+  end
+
+  test "successful index data" do
+    log_in_as(@user)
+    get index_path
+    assert_template 'index'
+    assert_select "tr", count: (User.count) + 1  # +1 car  un tr utilisé pour la legende
+  end
+
 end
